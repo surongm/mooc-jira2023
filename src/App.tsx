b88a586c85de0ec767d62abf63e79a9476569f7a
苏蓉敏 <surongmin@powerpms.com>
2023-01-04 10:57:44 +0800

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ function App() {
           {" "}
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-
         <a
           className="App-link"
           href="https://reactjs.org"
