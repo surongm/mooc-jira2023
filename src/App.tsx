a3019dc5a36ab03dd4f26d3c3ce36a7d740c81ad
苏蓉敏 <surongmin@powerpms.com>
2023-01-04 11:19:39 +0800

    fix: husky

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {" "}
-          Edit <code>src/App.tsx</code>
-        </p>
+        <p> Edit</p>
         <a
           className="App-link"
           href="https://reactjs.org"
